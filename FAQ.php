4e30fbe894d3899894a710aec721b027ffff8720
WenlongQiu <126044764+WenlongQiu@users.noreply.github.com>
2023-10-12 16:51:09 +1100

    Add files via upload

---
+++ FAQ.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>FAQ</title>
+    </head>
+    <style>
+        body{
+            margin: 0;
+        }
+        .center{
+            width: 800px;
+            margin: 0 auto;
+        }
+    </style>
+    <body>
+        <div class="center">
+            <h1>FAQ Help Center</h1>
+            <h3>FAQ 1: How can I track the status of my order?</h3>
+            <h3>Answer:</h3>
+            <p>You can easily track the status of your order by logging into your account and navigating to the "Order History" section. There, you'll find a list of all your recent orders along with their current status, such as "Processing," "Shipped," or "Delivered." You'll also receive email notifications with updates on your order status.</p>
+            <h3>FAQ 2: What payment methods are accepted for my purchases?</h3>
+            <h3>Answer:</h3>
+            <p>We accept a variety of payment methods to provide you with convenience and security. You can make payments using major credit and debit cards, including Visa, MasterCard, and American Express. Additionally, we offer the option to pay through secure digital wallets like PayPal and Apple Pay. Rest assured, all transactions are encrypted and processed securely to safeguard your financial information.</p>
+            <a href="landing.php"><button>Back</button></a>
+        </div>
+    </body>
+</html>
